8af47c16447b719c3375736562b76514810ad104
Ryan Conway <ryan@rjc.cc>
2022-09-02 14:55:41 +0100

    Disable some unused sample code for adding commands to the command palette.

--- src/extension.ts
+++ src/extension.ts
@@ -10,19 +10,18 @@ export function activate(context: vscode.ExtensionContext) {
 
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "better-status-bar" is now active!');
+  // console.log('Congratulations, your extension "better-status-bar" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('better-status-bar.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
+  // let disposable = vscode.commands.registerCommand('better-status-bar.helloWorld', () => {
+  //   // The code you place here will be executed every time your command is executed
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from Better Status Bar!');
-  });
-
-  context.subscriptions.push(disposable);
+  //   // Display a message box to the user
+  //   vscode.window.showInformationMessage('Hello World from Better Status Bar!');
+  // });
+  // context.subscriptions.push(disposable);
 
   // Add a status bar item to the right of the status bar, highest priority means it'll be the left most of the icons on the right.
   statusBarItem = vscode.window.createStatusBarItem(
