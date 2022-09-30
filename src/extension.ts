c61d01b7bd6e121f67c283f0c2a63c427ed9ca29
Ryan Conway <ryan@rjc.cc>
2022-09-30 19:14:56 +0100

    Add support for counting number of characters in the selections.

--- src/extension.ts
+++ src/extension.ts
@@ -54,35 +54,75 @@ export function updateStatusBarItemState() {
   }
 
   else {
-    // otherwise get the current editor state and update our statusbar accordingly.
+    // Otherwise get the current editor state and update our statusbar accordingly.
 
-    if (editor.selections.length > 1) {
-      var text = `${editor.selections.length} selections`;
-    }
-    else {
+    if (editor.selections.length === 1) {
+      // If we only have a single cursor, we can show the current line and column.
       var cursorPosition = editor.selection.active;
       var text = `Ln ${cursorPosition.line + 1}, Col ${cursorPosition.character + 1}`;
     }
+    else {
+      // Otherwise we just show the number of selections.
+      var text = `${editor.selections.length} selections`;
+    }
 
+    // If the start and end of the selection is on a different line or column, then
+    // we know we have something highlighted, and we want to show how many lines and
+    // characters are included.
     if (editor.selection.start.line !== editor.selection.end.line ||
       editor.selection.start.character !== editor.selection.end.character) {
-      // If the start and end of the selection is on a different line or column, then
-      // we know we have something highlighted, and we want to show how many lines are included.
 
-      var lineNumbers = new Set();
-
+      // Start with the number of unique lines in the selection range.
+      var uniqueLineNumbersInSelections = new Set();
       for (let selection of editor.selections) {
         // Generate a range of line numbers between the selection start and end, add to a set of unique line numbers.
-        range(selection.start.line, selection.end.line).forEach(item => lineNumbers.add(item));
+        range(selection.start.line, selection.end.line).forEach(item => uniqueLineNumbersInSelections.add(item));
+      }
+      var numLinesInSelections = uniqueLineNumbersInSelections.size;
+
+      var numLinesText = `${numLinesInSelections} line${numLinesInSelections > 1 ? 's' : ''}`;
+
+
+      // Next, calculate the number of characters across the selections.
+      // TODO: this is going to double count some characters if the user has multiple
+      // selections which span the same lines multiple times.
+      var numCharsInSelections = 0;
+
+      for (let selection of editor.selections) {
+        if (editor.selection.start.line === editor.selection.end.line) {
+          // If the selection starts and end of a single line, we can calculate the number of
+          // characters.
+          numCharsInSelections += editor.selection.end.character - editor.selection.start.character;
+        }
+        else {
+          // If the selection spans several lines, we need to work out the number of characters
+          // selected on the start line, and end line, and include all characters from all lines in between
+          // that the highlight will span.
+          var charsInEachLineInSelection = 0;
+          for (let lineNum of range(selection.start.line, selection.end.line)) {
+            if (lineNum === selection.start.line) {
+              charsInEachLineInSelection += (editor.document.lineAt(lineNum).text.length - editor.selection.start.character);
+            }
+            else if (lineNum === selection.end.line) {
+              charsInEachLineInSelection += editor.selection.end.character;
+            }
+            else {
+              charsInEachLineInSelection += editor.document.lineAt(lineNum).text.length;
+            }
+          };
+          numCharsInSelections += charsInEachLineInSelection;
+        }
       }
 
-      text = `${text} (${lineNumbers.size} lines selected)`;
+      var numCharsText = `${numCharsInSelections} chr${numCharsInSelections > 1 ? 's' : ''}`;
+
+      // Stick it all together and add to the text we want to show.
+      text = `${text} (${numLinesText} / ${numCharsText} selected)`;
     }
 
     statusBarItem.text = text;
     statusBarItem.show();
   }
-
 }
 
 /**
